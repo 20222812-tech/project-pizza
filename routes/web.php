2e87e140cc9888411f4cfc64937224fc42bb48df
20222812-tech <20222812@eaut.edu.vn>
2026-03-31 23:12:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::post('/nhanvien/store', [NhanVienController::class, 'store']);
 Route::get('/nhanvien/edit/{id}', [NhanVienController::class, 'edit']);
 Route::post('/nhanvien/update/{id}', [NhanVienController::class, 'update']);
 Route::post('/nhanvien/delete/{id}', [NhanVienController::class, 'destroy']);
+
+use App\Http\Controllers\SanPhamController;
+
+Route::get('/sanpham', [SanPhamController::class, 'index']);
+Route::get('/sanpham/create', [SanPhamController::class, 'create']);
+Route::post('/sanpham/store', [SanPhamController::class, 'store']);
+Route::get('/sanpham/edit/{id}', [SanPhamController::class, 'edit']);
+Route::post('/sanpham/update/{id}', [SanPhamController::class, 'update']);
+Route::post('/sanpham/delete/{id}', [SanPhamController::class, 'destroy']);
